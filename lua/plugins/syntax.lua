8539c2e3aa366fe198b8f4d1ec440e1fd555dfd8
dexslender <dexslender@proton.me>
2026-04-28 23:29:05 -0600

    update dependencies * swicth nvim-treesitter branch to main

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -4,14 +4,12 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
-            configs.setup({
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end
+        branch = "main",
+        opts = {
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        }
     },
     {
         'neovim/nvim-lspconfig',
